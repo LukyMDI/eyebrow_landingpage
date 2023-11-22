8ba7889843307b7bdcf79300488e0d10b7fa1892
LukyMDI <97931181+LukyMDI@users.noreply.github.com>
2023-11-22 15:18:37 -0300

    Alteração do sistema layout nas sections
    - Alteração no sistema de Grid para Flex nas sections
    - Alteração no tipo de posicionamente vertical na section SobreNos.jsx

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -8,17 +8,17 @@ export default function Home() {
         <>
             <section
                 id="home"
-                className="bg-[url('../../public/images/bg-1.webp')] bg-cover bg-no-repeat w-full h-screen min-h-[500px] grid grid-cols-1 grid-rows-3 justify-items-center"
+                className="bg-[url('../../public/images/bg-1.webp')] bg-cover bg-no-repeat w-full h-screen min-h-[500px] flex flex-col items-center justify-evenly"
             >
                 <div
-                    className={`${lato.className} flex font-playfair flex-col items-center text-white font-black text-3xl self-center col-span-1 mt-16`}
+                    className={`${lato.className} flex font-playfair flex-col items-center text-white font-black text-3xl self-center mt-16`}
                 >
                     <h1 className="text-lg">REALÇANDO A BELEZA</h1>
                     <h1>DO SEU OLHAR</h1>
                     <hr className="w-56 border-b-[3px] rounded border-c_purpleC" />
                 </div>
-                <div className="w-full flex justify-center col-span-1 z-0">
-                    <div className="relative w-5/6 h-4/6 border-4 border-solid border-c_purpleC rounded-md">
+                <div className="w-full h-1/5 flex justify-center z-0">
+                    <div className="relative w-5/6 border-4 border-solid border-c_purpleC rounded-md">
                         <Image
                             src={modelo_1}
                             objectFit="cover"
@@ -28,7 +28,7 @@ export default function Home() {
                         />
                     </div>
                 </div>
-                <div className="border-4 rounded-md border-white w-11/12 h-5/6 col-span-1 bg-black/70 p-5 overflow-auto box-border">
+                <div className="border-4 rounded-md border-white w-11/12 bg-black/70 p-5 overflow-auto box-border">
                     <div
                         className={`${lato.className} text-white text-center font-black text-lg`}
                     >
