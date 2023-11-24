a3be77654cfa8c6dbce1b65ac84b2a146d342a89
LukyMDI <97931181+LukyMDI@users.noreply.github.com>
2023-11-24 15:34:33 -0300

    Estruturação da section de Contato
    - Criado o botão para direcionar ao whatsapp

--- src/components/sections/Contato.jsx
+++ src/components/sections/Contato.jsx
@@ -1,8 +1,21 @@
+"use client";
+
+import Link from "next/link";
+
 export default function Contato() {
+    const linkWhats =
+        "https://wa.me/5582996910363/?text=funcionou corretamente";
+
+    const openWhatsappChat = () => {
+        window.open(linkWhats, "whatsapp", "width=400, height=600");
+    };
+
     return (
         <section id="contato" className="bg-c_purple">
             <div>
                 <h1>CONTATO</h1>
+                <Link href={linkWhats}>Clique</Link>
+                <button onClick={openWhatsappChat}>Clique</button>
             </div>
         </section>
     );
