336addd0d5b6fb1ddf0518fd519b241c4403b829
LukyMDI <97931181+LukyMDI@users.noreply.github.com>
2023-11-24 15:43:35 -0300

    Whatsapp Button
    - Estilização do elemento de button para o Whatsapp
    - Alteração de <Link> para <a> no redirecionamento

--- src/components/sections/Contato.jsx
+++ src/components/sections/Contato.jsx
@@ -1,21 +1,22 @@
-"use client";
-
 import Link from "next/link";
 
 export default function Contato() {
-    const linkWhats =
-        "https://wa.me/5582996910363/?text=funcionou corretamente";
-
-    const openWhatsappChat = () => {
-        window.open(linkWhats, "whatsapp", "width=400, height=600");
-    };
+    const linkWhats = "https://wa.me/5582996910363/?text=textourl";
 
     return (
-        <section id="contato" className="bg-c_purple">
+        <section
+            id="contato"
+            className="bg-c_purple flex flex-col items-center"
+        >
             <div>
                 <h1>CONTATO</h1>
-                <Link href={linkWhats}>Clique</Link>
-                <button onClick={openWhatsappChat}>Clique</button>
+                <a
+                    href={linkWhats}
+                    target="_blank"
+                    className="bg-green-600 text-white p-3 rounded"
+                >
+                    Clique Aqui senhor
+                </a>
             </div>
         </section>
     );
